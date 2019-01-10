7124c2a59f54ac3b3c0ce40deb7657b35f737d50
Unknown <breno_lopes_10@yahoo.com.br>
2019-01-10 11:36:08 -0200

    Inicio de config. do ambiente
    Instalação nodejs, bibliotecas possivelmente usadas e react.

---
+++ cliente/Components/paginaInicial.js
@@ -0,0 +1,20 @@
+    import React from 'react';
+
+    const botaoIniciar = props => {
+        return(
+            <div>
+
+            </div>
+        );
+    };
+
+    const background = props => {
+      return (
+          <div className="pagina-inicial">
+
+          </div>
+      );
+    };
+
+    export default background;
+    export default botaoIniciar;
---
+++ cliente/src/App.js
@@ -0,0 +1,14 @@
+  import React, { Component } from 'react';
+
+  import paginaInicial from '.././Components/paginaInicial';
+
+  class App extends Component {
+    render() {
+      return (
+            <paginaInicial/>
+
+      );
+    }
+  }
+
+  export default App;
