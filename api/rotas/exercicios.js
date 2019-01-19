9b88e747028308c3ed335bcdc7c51442dfd50769
Unknown <breno_lopes_10@yahoo.com.br>
2019-01-19 11:42:41 -0200

    inserindo documentos manualmente
    Inserindo documentos manualmente atraves da rota exercicios

--- api/rotas/exercicios.js
+++ api/rotas/exercicios.js
@@ -2,14 +2,23 @@
 
       //conexao do banco de dados
 
-      const conexao = mongoose.connect('mongodb://localhost:27017/playlearn');
+      const conexao = mongoose.connect('mongodb://localhost:27017/playlearn', { useNewUrlParser: true });
       if(conexao){
           console.log("conexao efetuada com sucesso!");
       }
 
       let Exercicios = mongoose.model('exercicio', {instrucao: String, nivel: String, numeroId: Number});
 
-         module.exports = function(app) {
+          var exercicio = new Exercicios({instrucao: 'Faça a variável ‘salario’ receber um valor digitado pelo usuário', nivel :'basico',numeroId: 5});
+
+          exercicio.save(function (err) {
+              if (err) throw err;
+              console.log('Dados salvos com sucesso!');
+          });
+
+
+
+      module.exports = function(app) {
              //Buscar respostas do bd e responder por aqui em formato json
             app.get('/exercicios', function (req, res) {
                 Exercicios.find(function (err, exercicio) {
@@ -18,5 +27,5 @@
                     console.log("Operação de recuperar dados do mongodb funcionou.");
                     res.send(JSON.stringify(exercicio));
                 });
-            })
+            });
         };
