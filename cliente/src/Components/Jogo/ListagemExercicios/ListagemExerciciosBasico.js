7ae67c66b0957f95b584598fb27c2bec02bab4e9
Unknown <breno_lopes_10@yahoo.com.br>
2019-01-21 10:11:35 -0200

    recebendo responses com axios nas listagens dos exercicios

--- cliente/src/Components/Jogo/ListagemExercicios/ListagemExerciciosBasico.js
+++ cliente/src/Components/Jogo/ListagemExercicios/ListagemExerciciosBasico.js
@@ -3,16 +3,35 @@
         import {Icon} from "semantic-ui-react";
         import {Link} from "react-router-dom";
         import './style.css';
+        import axios from 'axios';
+
 
         export default class ListagemExerciciosBasico extends Component {
+               constructor(props){
+                   super(props);
+                this.state = {
+                    exercicios: []
+                };
+
+                
+                axios
+                    .get('http://localhost:3001/exerciciosnivel/basico')
+                    .then(resultado =>{
+                        console.log(resultado.data);
+                        //this.setState({exercicios: resultado.data[0]});
+                    });
+
+               }
+
+
+
             render() {
                 return (
                     <div className="container">
                         <h3 className="text-center espacamentoTop">Exercicios do nivel Básico</h3>
                         <Link to="/niveis" ><Icon name="arrow circle left" size="big" color="black"/></Link>
                         <ListGroup className="espacamentoTop">
-                            <ListGroupItem tag="a" href="/exercicio/:numero" action> Listar exercicios em forma de lista por numero do exercicio</ListGroupItem>
-
+                            <ListGroupItem tag="a" href="#" action>{this.state.exercicios}</ListGroupItem>
                         </ListGroup>
 
                     </div>
