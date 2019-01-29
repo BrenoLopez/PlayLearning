e800d8feaef1eb78ed09912fa544abf0b20cca6c
Unknown <breno_lopes_10@yahoo.com.br>
2019-01-29 03:14:57 -0200

    conexão com banco de dados remoto

--- api/config/conexaoBd.js
+++ api/config/conexaoBd.js
@@ -1,6 +1,6 @@
     const mongoose = require ('mongoose');
 
-   const conexao = mongoose.connect('mongodb://localhost:27017/playlearn', { useNewUrlParser: true });
+   const conexao = mongoose.connect('mongodb://breno:playlearn123@ds117773.mlab.com:17773/playlearn', { useNewUrlParser: true });
 
     if(conexao){
         console.log("conexao efetuada com sucesso!");
